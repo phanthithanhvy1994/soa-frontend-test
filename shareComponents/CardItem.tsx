22cafc14437c74adbe0a34b789a8911e15a45a11
Vy Phan <phanthithanhvy1994@gmail.com>
2025-03-04 20:12:49 +0700

    feat: Done implement section 2

---
+++ shareComponents/CardItem.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { ArrowOutward, Instagram } from '@mui/icons-material'
+import { Box, Card, CardActionArea, CardContent, Typography } from '@mui/material'
+
+interface ImageCardProps {
+  imageUrl: string
+  author: string
+  date: string
+}
+
+const CardItem: React.FC<ImageCardProps> = ({ author, date, imageUrl }) => {
+  return (
+    <Card sx={{ borderRadius: 3, overflow: 'hidden', boxShadow: 3 }}>
+      <CardActionArea>
+        <Box
+          sx={{
+            width: '100%', // Chiếm toàn bộ chiều rộng
+            height: 0, // Chiều cao tự động
+            paddingTop: '100%', // Giữ tỷ lệ 16:9
+            position: 'relative'
+            // overflow: 'hidden'
+          }}
+        >
+          <img
+            src={imageUrl}
+            alt={author}
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover'
+            }}
+          />
+        </Box>
+        <CardContent
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            backgroundColor: '#5C5C5C',
+            color: 'white',
+            p: 1.5
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <Instagram fontSize='small' />
+            <Typography variant='body2'>{author}</Typography>
+          </Box>
+          <ArrowOutward fontSize='small' />
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+export default CardItem
---
+++ shareComponents/CaseGrid.tsx
@@ -0,0 +1,95 @@
+"use client";
+import Container from "@/components/Container";
+import TitleBlock from "@/shareComponents/TitleBlock";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+import useMediaQuery from '@mui/material/useMediaQuery'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+
+interface Case {
+  title: string;
+  subtitle: string;
+  cases: CaseItem[];
+}
+
+interface CaseItem {
+  category: string;
+  cta: string;
+  description: string;
+  tagline: string;
+  image: string;
+}
+
+const CaseGrid = ({ title, subtitle, cases }: Case): JSX.Element => {
+  const isMobile = useMediaQuery('(max-width:600px)')
+  const newCases = cases.map((item, index) => ({
+    ...item,
+    image: item.image || `/images/case${index + 1}.png`,
+  }));
+
+  return (
+    <Container>
+      <Box textAlign="left" py={5}>
+        <TitleBlock title={title} subtitle={subtitle} />
+        <Grid container spacing={3} justifyContent="center">
+          {newCases.map((item, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Card
+                sx={{
+                  borderRadius: 3,
+                  overflow: "hidden",
+                  boxShadow: 3,
+                  maxWidth: { xs: "100%", sm: 345 },
+                  margin: "0 auto",
+                  transform:
+                    index % 2 === 0 && !isMobile? "translateY(30px)" : "translateY(-10px)",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={item.image}
+                  alt={item.category}
+                />
+                <CardContent>
+                  <Typography variant="caption" color="orange" gutterBottom>
+                    {item.category}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    component="h2"
+                    fontWeight="bold"
+                    gutterBottom
+                  >
+                    {item.tagline}
+                  </Typography>
+                  <Typography variant="body2" gutterBottom>
+                    {item.description}
+                  </Typography>
+                  <Box mt={2}>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      endIcon={<ArrowOutwardIcon />}
+                    >
+                      {item.cta}
+                    </Button>
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+export default CaseGrid;
